0b3329c919b3434913843941175436e1adf520b9
ptg0811 <ptg0811@gmail.com>
2025-03-25 04:30:25 +0900

    (add)secrets

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-AWS_ACCESS_KEY = "AKIAVOSIS7OFYWNZPLPH"
-AWS_SECRET_KEY = "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"
-BUCKET_NAME = "carbonlink-demo"
+import streamlit as st
+
+AWS_ACCESS_KEY = st.secrets["AWS_ACCESS_KEY"]
+AWS_SECRET_KEY = st.secrets["AWS_SECRET_KEY"]
+BUCKET_NAME = st.secrets["BUCKET_NAME"]
